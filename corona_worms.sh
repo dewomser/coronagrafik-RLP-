db51a13511d0b235f4ffa452870ae6baf88df17c
dewomser <webmaster@untergang.de>
2021-04-02 11:15:39 +0200

    Quelle hat sich geändert.

--- corona_worms.sh
+++ corona_worms.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-#Pfad /home/foo/bin/Pandemie1 muss durch eigenen ersetzt werden.
-cd /home/foo/bin/Pandemie1
-rm Listen_Corona_RLP.xlsx*
+
+cd /home/karl/bin/Pandemie1
+rm Fallzahlen_und_Todesfaelle_Corona_RLP.xlsx
 sleep 2
-wget https://msagd.rlp.de/fileadmin/msagd/Gesundheit_und_Pflege/GP_Dokumente/Informationen_zum_Coronavirus/Listen_Corona_RLP.xlsx
+#wget https://msagd.rlp.de/fileadmin/msagd/Gesundheit_und_Pflege/GP_Dokumente/Informationen_zum_Coronavirus/Listen_Corona_RLP.xlsx
+wget https://lua.rlp.de/fileadmin/lua/Downloads/Corona/Listen/Fallzahlen_und_Todesfaelle_Corona_RLP.xlsx
 sleep 2
-unoconv -f csv -o /home/foo/bin/Pandemie1/Listen_Corona_RLP_ungekippt.csv /home/karl/bin/Pandemie1/Listen_Corona_RLP.xlsx 
+unoconv -f csv -o /home/karl/bin/Pandemie1/Listen_Corona_RLP_ungekippt.csv /home/karl/bin/Pandemie1/Fallzahlen_und_Todesfaelle_Corona_RLP.xlsx
 sleep 2
-/home/foo/bin/Pandemie1/csvkippen.sh
+/home/karl/bin/Pandemie1/csvkippen.sh
 sleep 2
 gnuplot corona.gp
-# evtl. muss libreoffice noch gekillt werden
-# killall soffice.bin
+gnuplot corona-kl.gp
+gnuplot corona-lu.gp
+gnuplot corona-mz.gp
+gnuplot corona-tr.gp
+gnuplot corona-ko.gp
+gnuplot corona-sp.gp
+killall soffice.bin
